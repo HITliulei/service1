ef8f8d7b63f8b6b54adee0b0889b518f73f7dd05
liulei <liulei13041x@163.com>
2020-02-10 16:51:42 +0800

    version1.0

---
+++ src/main/java/com/test/service1/controller/service1Provide.java
@@ -0,0 +1,48 @@
+package com.test.service1.controller;
+
+import com.test.service1.entity.User;
+import com.test.service1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author Lei
+ * @Date 2020/2/9 15:21
+ * @Version 1.0
+ */
+
+@RestController
+@RequestMapping("/userService")
+public class service1Provide {
+
+    @Value("${eureka.instance.metadata-map.version}")
+    private String version;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/getServiceInfo")
+    public String getVersion(){
+        return this.version;
+    }
+    @GetMapping("/user/{id}")
+    public User findOne(@PathVariable("id")Long id){
+        return this.userRepository.getOne(id);
+    }
+
+
+    @PutMapping("/puttest")
+    public String puttest(@RequestBody User user){
+        System.out.println("      " + user.toString());
+        return "success put test";
+    }
+
+    @GetMapping("/alluser")
+    public List<User> getall(){
+        return this.userRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/test/service1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.test.service1.repository;
+
+import com.test.service1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
